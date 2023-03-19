aee73228522903dd86869d4f23c52295bba9db8b
Bharat Rajani <bharat.ramrajani@gmail.com>
2023-03-19 23:32:27 +0530

    corrections

--- internal/configurer/configure.go
+++ internal/configurer/configure.go
@@ -2,7 +2,6 @@ package configurer
 
 import (
 	"context"
-	"fmt"
 	"github.com/bharat-rajani/go-polls/internal/service"
 	"github.com/spf13/viper"
 )
@@ -22,7 +21,7 @@ func loadConfig() (*service.ServiceConfig, error) {
 	viper.SetConfigFile("./config.yml")
 	err := viper.ReadInConfig() // Find and read the config file
 	if err != nil {             // Handle errors reading the config file
-		panic(fmt.Errorf("fatal error config file: %w", err))
+		return nil, err
 	}
 	var serviceConfig *service.ServiceConfig
 	err = viper.UnmarshalKey("service", &serviceConfig)
