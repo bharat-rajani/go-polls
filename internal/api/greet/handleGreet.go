3b31363cf66547ed8a76d48c6d06201bbe0ccf14
Bharat Rajani <bharat.ramrajani@gmail.com>
2023-04-02 15:09:04 +0530

    fix lint issue

--- internal/api/greet/handleGreet.go
+++ internal/api/greet/handleGreet.go
@@ -8,6 +8,7 @@ import (
 
 func HandleGreeter(s *service.Service) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		s.Log.Info().Msg("received request for greet ")
 		_, err := w.Write([]byte(`HelloResponse`))
 		if err != nil {
 			return
